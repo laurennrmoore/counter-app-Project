d42fd5579a34f5631bf62e5306cb6b10e1e4a346
lmoore <lmooreintech2021@gmail.com>
2021-05-14 11:04:07 -0400

    ampush

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react'
+import Plus from './components/Plus';
+import Minus from './components/Minus';
+import InputNum from './components/InputNum';
+import Reset from './components/Reset';
 
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+countUp = () => {
+  this.setState(
+    {count: this.state.count + 1}
+  )
 }
 
-export default App;
+countDown = () => {
+  this.setState(
+    {count: this.state.count - 1}
+  )
+}
+
+inputNum = (number) =>{
+  let value = parseInt(number)
+  this.setState(
+    {count: this.state.count + value}
+  )
+}
+
+reset = () => {
+  this.setState({
+    count: 0
+  })
+}
+
+render() {
+  return(
+    <div className="App">
+      <div className="container">
+        <p>Tracking Time: Time is always moving along!</p>
+        <h4>{this.state.count}</h4>
+      </div>
+      <div className="container-two">
+        <Plus countUp = { this.countUp } />
+        <Minus countDown = { this.countDown } />
+      </div>
+      <div className ="container-three">
+        <InputNum inputNum = { this.inputNum } />
+        <Reset reset = { this.reset } />
+      </div>
+    </div>
+  )
+}
+}
+
+export default App
